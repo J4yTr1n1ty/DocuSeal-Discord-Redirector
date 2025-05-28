09c1a049e20d3ab61cff29d958145c9094f6d116
Jay <jay@projectchaos.ch>
2025-05-29 01:04:14 +0200

    feat(docker): added docker compatibility

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/J4yTr1n1ty/DocuSeal-Discord-Redirector
 
-go 1.24.3
+go 1.21
 
 require (
 	github.com/gorilla/mux v1.8.1
--- pkg/api/router.go
+++ pkg/api/router.go
@@ -1,12 +1,18 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/J4yTr1n1ty/DocuSeal-Discord-Redirector/pkg/docuseal"
 	"github.com/gorilla/mux"
 )
 
 func InitRouter() *mux.Router {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
 
 	router.HandleFunc("/incoming/{key}", docuseal.Handle)
 
